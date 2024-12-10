58658209f801c42c2ad6af671195000f0eec8067
Abdullahi Yusuf <abdullahiyusuf182@gmail.com>
2024-12-10 07:44:49 +0100

    Added stock controller to get stocks and a particular stock.

---
+++ StockComm2/Controllers/StockController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockComm.Data;
+
+namespace StockComm.Controllers
+{
+    [ApiController]
+    [Route("api/stock")]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        public StockController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllStocks()
+        {
+            var stocks = _db.Stocks.ToList();
+            
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStockById([FromRoute] int id)
+        {
+            var stockFromDb = _db.Stocks.Find(id);
+
+            return Ok(stockFromDb);
+        }
+    }
+}
