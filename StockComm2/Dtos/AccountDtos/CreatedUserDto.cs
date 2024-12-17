b91051f6842ab55e08c8ab45367b59f19db0d301
Abdullahi Yusuf <abdullahiyusuf182@gmail.com>
2024-12-17 12:52:45 +0100

    JWT services were added and tokens are being generated when user is created.

---
+++ StockComm2/Dtos/AccountDtos/CreatedUserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace StockComm.Dtos.AccountDtos
+{
+    public class CreatedUserDto
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
+    }
+}
